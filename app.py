daa76188244eaac23d8aa2a240648e0be45c277c
brendan <lobrendan@gmail.com>
2022-01-27 15:25:06 -0800

    updated to use flask sessions to operate with multiple users

--- app.py
+++ app.py
@@ -8,19 +8,23 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
-responses = []
+
 
 @app.get('/')
 def survey_start():
     """Renders survey start page"""
 
+    # can we clarify why this needs to be inside a route rather than in the global scope? bc it needs to be part of a request?
+    session['responses'] = []
+
     survey_title = survey.title
     survey_instructions = survey.instructions
-  
+
     return render_template(
-        "survey_start.html", 
-        title = survey_title, 
-        instructions = survey_instructions)
+        "survey_start.html",
+        title=survey_title,
+        instructions=survey_instructions)
+
 
 @app.post('/begin')
 def redirect_to_questions():
@@ -28,22 +32,24 @@ def redirect_to_questions():
 
     return redirect("/questions/0")
 
+
 @app.get('/questions/<int:question_num>')
 def get_question(question_num):
     """Render question page"""
 
-    question_instance = survey.questions[question_num] 
+    question_instance = survey.questions[question_num]
     question = question_instance.question
     choices = question_instance.choices
     allow_text = question_instance.allow_text
     print(f"allow text {allow_text}")
 
     return render_template(
-        "question.html", 
-        question=question, 
-        choices=choices, 
-        allow_text=allow_text, 
-        question_num = question_num)
+        "question.html",
+        question=question,
+        choices=choices,
+        allow_text=allow_text,
+        question_num=question_num)
+
 
 @app.post('/answer')
 def add_response_and_redirect():
@@ -51,10 +57,15 @@ def add_response_and_redirect():
     If last question, redirect to competion page.
     Else, increment question number and redirect next question page"""
 
+    responses = session['responses']
+
     if request.form.get('text_answer'):
-        responses.append({request.form['answer'] : request.form['text_answer']})
+        responses.append(
+            {request.form['answer']: request.form['text_answer']})
     else:
-         responses.append(request.form['answer'])
+        responses.append(request.form['answer'])
+
+    session['responses'] = responses
 
     question_num = int(request.form['question_num']) + 1
 
@@ -67,5 +78,5 @@ def add_response_and_redirect():
 @app.get('/completion')
 def thank_user():
     """Render completion page"""
-    print(responses)
+    print(session['responses'])
     return render_template("completion.html")
