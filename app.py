f0da00256456bc19ceecb44abcce9819edc6b168
brendan <lobrendan@gmail.com>
2022-01-27 17:09:01 -0800

    finished alerts

--- app.py
+++ app.py
@@ -12,9 +12,10 @@ debug = DebugToolbarExtension(app)
 
 @app.get('/')
 def survey_start():
-    """Renders survey start page"""
+    """Renders survey start page and initializes session cookies responses & 
+    current_question"""
 
-    # can we clarify why this needs to be inside a route rather than in the global scope? bc it needs to be part of a request?
+    # can check len(session['responses']) to check the right question. could put the reset values in the begin route
     session['responses'] = []
     session['current_question'] = 0
 
@@ -36,14 +37,24 @@ def redirect_to_questions():
 
 @app.get('/questions/<int:question_num>')
 def get_question(question_num):
-    """Render question page"""
+    """Render question page and prevents users from accessing questions that 
+    are not valid"""
 
+    # if user tries to directly access a question, but has completed the survey,
+    # they will be redirected to the completion page
     if (session['current_question'] == len(survey.questions)):
+        flash("You've already completed the survey, thanks!")
         return redirect('/completion')
+
+    # if user tries directly accessing a question out of order, they will be
+    # redirected to their correct question page
     elif (question_num != session['current_question']):
         print("Redirecting to current page")
+        flash(
+            "You were trying to access an invalid question, please answer this one")
         return redirect(f"/questions/{session['current_question']}")
 
+    # can clean this up by passing the question instance directly to Jinja
     question_instance = survey.questions[question_num]
     question = question_instance.question
     choices = question_instance.choices
@@ -63,19 +74,21 @@ def add_response_and_redirect():
     If last question, redirect to competion page.
     Else, increment question number and redirect next question page"""
 
-    responses = session['responses']
     session['current_question'] += 1
 
+    responses = session['responses']
+
+    # stores data into session['responses']
     if request.form.get('text_answer'):
         responses.append(
             {request.form['answer']: request.form['text_answer']})
     else:
         responses.append(request.form['answer'])
 
+    # rebinding triggers session to save the updated values
     session['responses'] = responses
 
-    # question_num = int(request.form['question_num']) + 1
-
+    # directing user to the correct page
     if (session['current_question'] == len(survey.questions)):
         return redirect('/completion')
     else:
@@ -85,5 +98,5 @@ def add_response_and_redirect():
 @app.get('/completion')
 def thank_user():
     """Render completion page"""
-    print(session['responses'])
+
     return render_template("completion.html")
